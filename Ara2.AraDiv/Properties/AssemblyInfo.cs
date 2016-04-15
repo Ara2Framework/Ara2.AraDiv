fa90a29d5d842436affa3f5ec45cbb745bd822eb
HenriqueVictorino <henriquevictorino@gmail.com>
2016-04-15 11:28:47 -0300

    Review

--- Ara2.AraDiv/Properties/AssemblyInfo.cs
+++ Ara2.AraDiv/Properties/AssemblyInfo.cs
@@ -6,18 +6,18 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Ara2.AraDiv")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("AraDiv")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Ara2Framework")]
 [assembly: AssemblyProduct("Ara2.AraDiv")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright (c) 2010-2016, Rafael Leonel Pontani. All rights reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[assembly: ComVisible(true)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("ddf8a092-06fc-41b7-aa14-d63869fdc306")]
